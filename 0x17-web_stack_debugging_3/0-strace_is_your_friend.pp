b3657739f7cf352d411e05d4b736b0e227e09f1c
kiggund <abdulbasitkiggundu@gmail.com>
2024-06-14 14:29:22 +0000

    update file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-# This Puppet script is a response to an issue identified using strace to debug a 500 error from Apache, specifically within a WordPress installation
+# This Puppet script is a response to an issue identified using strace to debug a 500 error from Apache, 
+# specifically within a WordPress installation
 # The script automates the fix by using the sed command to replace 'phpp' with 'php' in the 'wp-settings.php' file
 # This solution is more scalable and maintainable than a Bash script
 

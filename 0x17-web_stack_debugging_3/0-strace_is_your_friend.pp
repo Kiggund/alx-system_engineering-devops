0911686a9f0f53ad00bc5cd690cd419673971524
kiggund <abdulbasitkiggundu@gmail.com>
2024-06-14 14:26:34 +0000

    update file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,8 @@
-exec { 'fix-apache-error':
-  command => '/bin/echo "Fixing Apache error"',
-  onlyif  => '/usr/bin/test ! -f /etc/apache2/conf-available/fixed.conf',
-  path    => ['/bin', '/usr/bin'],
-}
-file { '/etc/apache2/conf-available/fixed.conf':
-  ensure  => file,
-  content => "# Configuration fixed\n",
-  require => Exec['fix-apache-error'],
-}
-service { 'apache2':
-  ensure    => running,
-  subscribe => File['/etc/apache2/conf-available/fixed.conf'],
+# This Puppet script is a response to an issue identified using strace to debug a 500 error from Apache, specifically within a WordPress installation
+# The script automates the fix by using the sed command to replace 'phpp' with 'php' in the 'wp-settings.php' file
+# This solution is more scalable and maintainable than a Bash script
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
